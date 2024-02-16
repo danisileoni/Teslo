01057e309422914aa1ce6c11e0985bc5308634d7
Daniele <danielesileoni4@gmail.com>
2024-02-16 18:02:11 -0300

    uploads files in db

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -8,13 +8,23 @@ import {
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/fileNamer.helper';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file', { fileFilter: fileFilter }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  )
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
     if (!file) {
       throw new BadRequestException('Make sure that the file is an image');
--- src/files/helpers/fileNamer.helper.ts
+++ src/files/helpers/fileNamer.helper.ts
@@ -1,4 +1,8 @@
-export const fileFilter = (
+import { v4 as uuid } from "uuid";
+
+
+
+export const fileNamer = (
   req: Express.Request,
   file: Express.Multer.File,
   // eslint-disable-next-line @typescript-eslint/ban-types
@@ -7,11 +11,8 @@ export const fileFilter = (
   if (!file) return callback(new Error('File is empty'), false);
 
   const fileExtension = file.mimetype.split('/')[1];
-  const validExtensions = ['jpg', 'jpeg', 'png', 'gif'];
 
-  if (validExtensions.includes(fileExtension)) {
-    return callback(null, true);
-  }
+  const fileName = `${uuid()}.${fileExtension}`;
 
-  callback(null, true);
+  callback(null, fileName);
 };
