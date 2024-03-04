72dd3bcca18c4f355c24c39e2f6042d3767dac5b
Daniele <danielesileoni4@gmail.com>
2024-03-03 21:19:08 -0300

    Creating WS and connecting on the client

---
+++ src/messages-ws/messages-ws.gateway.ts
@@ -0,0 +1,24 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
+
+@WebSocketGateway({ cors: true })
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private readonly messagesWsService: MessagesWsService) {}
+
+  handleConnection(client: Socket) {
+    this.messagesWsService.registerClient(client);
+    console.log({ connected: this.messagesWsService.getConnectedClients() });
+  }
+
+  handleDisconnect(client: Socket) {
+    this.messagesWsService.removeClient(client.id);
+    console.log({ connected: this.messagesWsService.getConnectedClients() });
+  }
+}
