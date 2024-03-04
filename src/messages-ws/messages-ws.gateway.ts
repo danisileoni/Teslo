93918bde5c29bd9a6e11fc5389325b124cd9d499
Daniele <danielesileoni4@gmail.com>
2024-03-04 00:16:08 -0300

    Connect with front finalized

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,24 +1,70 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  @WebSocketServer() wss: Server;
 
-  handleConnection(client: Socket) {
-    this.messagesWsService.registerClient(client);
-    console.log({ connected: this.messagesWsService.getConnectedClients() });
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authorization as string;
+    let payload: JwtPayload;
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
 
   handleDisconnect(client: Socket) {
     this.messagesWsService.removeClient(client.id);
-    console.log({ connected: this.messagesWsService.getConnectedClients() });
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
+
+  @SubscribeMessage('message-form-client')
+  onMessageFromClient(client: Socket, payload: NewMessageDto) {
+    //!emit unique client
+    // client.emit('message-form-server', {
+    //   fullName: 'Soy io jiji',
+    //   message: payload.message || 'no-message!!',
+    // });
+
+    // client.broadcast.emit('message-form-server', {
+    //   fullName: 'Soy io jiji',
+    //   message: payload.message || 'no-message!!',
+    // });
+
+    this.wss.emit('message-form-server', {
+      fullName: this.messagesWsService.getUserFullName(client.id),
+      message: payload.message || 'no-message!!',
+    });
   }
 }
